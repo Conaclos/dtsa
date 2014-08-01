5821bfbe069822dcad0baea0c90de5c35878fd35
Conaclos <victorien.elvinger@gmail.com>
2014-08-01 14:26:00 +0200

    [Mapper] Removing uneeded classes.

--- mapper/src/main/java/dtsa/mapper/cloud/mapped/response/ServiceResponse.java
+++
@@ -1,9 +0,0 @@
-package dtsa.mapper.cloud.mapped.response;
-
-import dtsa.util.communication.base.Response;
-
-
-public abstract class ServiceResponse 
-	extends Response <ServiceResponseVisitor> {
-
-}
