7c7ed299328de87399548294e24165a477095d16
Conaclos <victorien.elvinger@gmail.com>
2014-08-18 10:39:05 +0200

    Fix space trailing

--- java_utils/src/main/java/dtsa/util/aws/AWS.java
+++ java_utils/src/main/java/dtsa/util/aws/AWS.java
@@ -11,28 +11,28 @@ import dtsa.util.configuration.UnmatchableTypeException;
 import dtsa.util.configuration.UnparsableException;
 
 /**
- * 
+ *
  * @description High Level interface with AWS.
  * @author Victorien ELvinger
  * @date 2014
  *
  */
 public class AWS {
-	
+
 // Creation
 	/**
-	 * 
+	 *
 	 * @param aConfiguration - service configurations.
 	 */
 	public AWS (AWSConfiguration aConfiguration) {
 		configuration = aConfiguration;
-		
+
 		assert configuration == aConfiguration: "ensure: `configuration' set with `aConfiguration'";
 	}
-	
+
 // Access
 	/**
-	 * 
+	 *
 	 * @return Pool created from `configuration'.
 	 * @throws UnparsableException
 	 * @throws UnmatchableTypeException
@@ -40,62 +40,63 @@ public class AWS {
 	public EC2InstancePool newInstancePool () throws UnparsableException, UnmatchableTypeException {
 		return new EC2InstancePool (configuration.getEc2 (), new AmazonEC2Client (defaultCredentials ()));
 	}
-	
+
 	/**
-	 * 
+	 *
 	 * @return Configured bucket.
 	 * @throws AmazonServiceException
 	 * @throws UnparsableException
 	 * @throws UnmatchableTypeException
 	 */
 	public S3Bucket defaultBucket () throws AmazonServiceException, UnparsableException, UnmatchableTypeException {
+		assert configuration.getS3 () != null: "require: s3 configuration exists.";
 		@Nullable S3Bucket result;
-		
+
 		result = defaultBucketCache;
 		if (result == null) {
 			result = new S3Bucket (configuration.getS3 (), new AmazonS3Client (defaultCredentials ()));
 			defaultBucketCache = result;
 		}
-		
+
 		assert result != null;
 		return result;
 	}
-	
+
 // Implementation
 	/**
 	 * AWS configuration
 	 */
 	protected AWSConfiguration configuration;
-	
-// Implementation (S3)	
+
+// Implementation (S3)
 	/**
 	 * Cache for `defaultBucket'.
 	 */
 	protected @Nullable S3Bucket defaultBucketCache;
-	
+
 // Implementation (Credentials)
 	/**
 	 * Retrieve AWScredentials from AWS local profile.
 	 * Profile name is specified into the aws configuration file with `profileKey'.
-	 * @throws UnmatchableTypeException 
+	 * @throws UnmatchableTypeException
 	 * @throws UnparsableException
 	 */
 	protected AWSCredentials defaultCredentials () throws UnparsableException, UnmatchableTypeException  {
 		@Nullable AWSCredentials result;
-		
+
 		result = defaultCredentialsCache;
 		if (result == null) {
 			result = new ProfileCredentialsProvider (configuration.getProfile ()).getCredentials ();
 			defaultCredentialsCache = result;
 		}
-		
+
 		assert result != null: "check: credentials exists";
 		return result;
 	}
-	
+
 	/**
 	 * Cache for `defaultCredentials'.
 	 */
 	protected @Nullable AWSCredentials defaultCredentialsCache;
-	
+
 }
--- mapper/src/main/java/dtsa/mapper/cloud/mapped/base/MappedProxy.java
+++ mapper/src/main/java/dtsa/mapper/cloud/mapped/base/MappedProxy.java
@@ -1,5 +1,6 @@
 package dtsa.mapper.cloud.mapped.base;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,7 +10,6 @@ import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 import java.util.function.Function;
-import java.io.BufferedReader;
 
 import dtsa.util.annotation.Nullable;
 import dtsa.util.communication.base.RepeatableTask;
@@ -21,35 +21,35 @@ import dtsa.util.communication.listener.ConvertibleObjectListener;
 import dtsa.util.communication.writer.ConvertibleObjectWriter;
 
 
-public class MappedProxy 
+public class MappedProxy
 	extends RepeatableTask {
-	
+
 // Creation
 	/**
-	 * 
+	 *
 	 * @param aIp - Service IP
 	 * @param aPort - Service port
 	 */
-	public MappedProxy (int aPort, ServiceAvailability aAvailabilty, 
+	public MappedProxy (int aPort, ServiceAvailability aAvailabilty,
 			Function <BufferedReader, ConvertibleObjectListener <? extends Response <? extends ResponseVisitor>>> aListenerFactory,
 			Function <BufferedWriter, ConvertibleObjectWriter <Request <? extends RequestVisitor>>> aWriterFactory) {
-		
+
 		port = aPort;
 		availabilty = aAvailabilty;
 		listenerFactory = aListenerFactory;
 		writerFactory = aWriterFactory;
-		
+
 		lock = new ReentrantLock ();
 		initializedCondition = lock.newCondition ();
-		
+
 		availabilty.start ();
 	}
-	
+
 // Thread operation
 	@Override
 	public void interrupt () {
 		super.interrupt ();
-		
+
 		@Nullable Socket localSocket = socket;
 		if (localSocket != null) {
 			try {
@@ -60,44 +60,44 @@ public class MappedProxy
 				e.printStackTrace();
 			}
 		}
-		
+
 		@Nullable RepeatableTask temp;
 		temp = listener;
 		if (temp != null) {
 			temp.interrupt ();
 		}
-		
+
 		temp = writer;
 		if (temp != null) {
 			temp.interrupt ();
 		}
 	}
-	
+
 // Access
 	/**
 	 * Get and remove the oldest input.
 	 * Wait the initialization of the communication until `aInitailzedTimeout'.
 	 * ANd then if no input is available, then wait one until `aTimeout'.
-	 * 
+	 *
 	 * @SideEffect
-	 * 
+	 *
 	 * @param aInitailzedTimeout - in milliseconds
 	 * @param aTimeout - in seconds
-	 * 
+	 *
 	 * @return Oldest input or null if `timeout' elapses before an input is available.
 	 * @throws InterruptedException - `aInitailzedTimeout' or `aTimeout' reached.
 	 */
 	public @Nullable Response <? extends ResponseVisitor> maybeNext (long aInitailzedTimeout, long aTimeout) throws InterruptedException {
 		@Nullable ConvertibleObjectListener <? extends Response <? extends ResponseVisitor>> localListener;
 		@Nullable Response <? extends ResponseVisitor> result;
-		
+
 		localListener = listener;
 		if (localListener != null) {
 			result = localListener.maybeNext (aTimeout);
 		}
 		else {
 			initializedCondition.wait (aInitailzedTimeout);
-			
+
 			localListener = listener;
 			if (localListener != null) {
 				result = localListener.maybeNext (aTimeout);
@@ -106,10 +106,10 @@ public class MappedProxy
 				result = null;
 			}
 		}
-		
+
 		return result;
 	}
-	
+
 // Other
 	/**
 	 * Transmitt `aObject'.
@@ -118,75 +118,75 @@ public class MappedProxy
 	 */
 	public void write (Request <? extends RequestVisitor> aObject) throws InterruptedException {
 		@Nullable ConvertibleObjectWriter <Request <? extends RequestVisitor>> localWriter;
-		
+
 		lock.lock ();
-		
+
 		try {
 			localWriter = writer;
 			while (localWriter == null) {
 				initializedCondition.await ();
 				localWriter = writer;
 			}
-			
+
 			System.out.println ("writing");
-			
+
 			localWriter.write (aObject);
 		}
 		finally {
 			lock.unlock ();
 		}
 	}
-	
+
 // Implementation
 	/**
 	 * Lock
 	 */
 	protected Lock lock;
-	
+
 	/**
 	 * COndition variable for initialization.
 	 */
 	protected Condition initializedCondition;
-	
+
 	/**
 	 * Service port.
 	 */
 	protected int port;
-	
+
 	/**
 	 * Availability notifier.
 	 */
 	protected ServiceAvailability availabilty;
-	
+
 	/**
 	 * Listener of response.
 	 */
 	protected Function <BufferedReader, ConvertibleObjectListener <? extends Response <? extends ResponseVisitor>>> listenerFactory;
-	
+
 	/**
 	 * Factory for {@link #listener}
 	 */
 	protected Function <BufferedWriter, ConvertibleObjectWriter <Request <? extends RequestVisitor>>> writerFactory;
-	
+
 	/**
 	 * Factory for {@link #writer}
 	 */
 	protected @Nullable ConvertibleObjectListener <? extends Response <? extends ResponseVisitor>> listener;
-	
+
 	/**
 	 * Listener of request.
 	 */
 	protected @Nullable ConvertibleObjectWriter <Request <? extends RequestVisitor>> writer;
-	
+
 	/**
 	 * Socket of the service client.
 	 */
 	protected @Nullable Socket socket;
-	
+
 	@Override
-	protected void initialize () {	
+	protected void initialize () {
 		Socket localSocket;
-		
+
 		try {
 			availabilty.waitAvailability ();
 		}
@@ -194,15 +194,15 @@ public class MappedProxy
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-		
+
 		try {
 			System.out.println (availabilty.getIp () + ":" + port);
-			
+
 			localSocket = new Socket (availabilty.getIp (), port);
 			socket = localSocket;
 			listener = listenerFactory.apply (new BufferedReader (new InputStreamReader (localSocket.getInputStream ())));
 			writer = writerFactory.apply (new BufferedWriter (new OutputStreamWriter (localSocket.getOutputStream ())));
-			
+
 			listener.start ();
 			writer.start ();
 		}
@@ -210,17 +210,17 @@ public class MappedProxy
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		System.out.println ("available");
-		
+
 		lock.lock ();
 		initializedCondition.signalAll ();
 		lock.unlock ();
 	}
-	
+
 	@Override
 	protected void process () {
 		isActive.set (false);
 	}
-	
+
 }
